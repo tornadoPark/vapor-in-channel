0ed129998fa2dc9c018ac3e9b269b69bb83ca19e
tornadoPark <57586390+tornadoPark@users.noreply.github.com>
2023-05-15 14:24:46 +0800

    壁面温度初始值的解释
    使用肋片解析解
    需要确定传热系数

--- getIniTemp.m
+++ getIniTemp.m
@@ -2,7 +2,8 @@ function [y] = getIniTemp (x)
 
 global ThetaScale
 
-h = 82050;
+% h = 82050; % from RTDs in porous zone
+h = 33500; % from experiment
 
 % y = (0.39*5+100-92)*cosh(4.68656*(h^0.5)*(x-0.0072))*sech(0.0337432*(h^0.5));
 y = (4+100-92)*cosh(4.68656*(h^0.5)*(x-0.0072))*sech(0.0337432*(h^0.5));
